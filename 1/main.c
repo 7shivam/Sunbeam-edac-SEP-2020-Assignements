faaa9875bfc82442a740789aea67209dfefec622
Shivam <shivamrathore910@gmail.com>
2020-09-25 13:56:34 +0530

    1. First Assignment

---
+++ 1/main.c
@@ -0,0 +1,23 @@
+
+#include <string.h>
+#include <stdio.h>
+
+int main () {
+   char str[80];
+   const char s[2] = ",";
+   char *token;
+   printf("Please input strings\n");
+   scanf("%s",&str);
+
+   /* get the first token */
+   token = strtok(str, s);
+
+   /* walk through other tokens */
+   while( token != NULL ) {
+      printf( " %s\n", token );
+
+      token = strtok(NULL, s);
+   }
+
+   return(0);
+}
